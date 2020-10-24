68598d2bdc4e264419724ecfd102364a251236f9
Sagar Gupta <sagar.kr.gupta@outlook.com>
2020-10-24 18:09:53 +0545

    part 5 => Split razor component into separate files

---
+++ BlazorServer101/Pages/CounterBase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorServer101.Pages
+{
+    public class CounterBase :ComponentBase
+    {
+        protected int currentCount = 0;
+        protected string fontName;
+        //'Segoe UI'
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        protected void ChangeFont()
+        {
+            if (fontName is null || fontName.Equals("Segoe UI"))
+                fontName = "fantasy";
+            else
+                fontName = "Segoe UI";
+        }
+    }
+}
---
+++ BlazorServer101/Pages/_Counter.razor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer101.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        string fontName;
+        //'Segoe UI'
+
+        private void IncrementCount()
+        {
+            currentCount++;
+        }
+
+        void ChangeFont()
+        {
+            if (fontName is null || fontName.Equals("Segoe UI"))
+                fontName = "fantasy";
+            else
+                fontName = "Segoe UI";
+        }
+    }
+}
