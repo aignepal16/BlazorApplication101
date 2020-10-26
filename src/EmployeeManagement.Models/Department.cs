687d8bb51b8287fec1e34f03aeecc6910f331694
Sagar Gupta <sagar.kr.gupta@outlook.com>
2020-10-26 21:23:26 +0545

    19 => implemented API , BlazorUI of EmployeeManagement Project

---
+++ src/EmployeeManagement.Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public virtual IEnumerable<Employee>  Employees { get; set; }
+
+    }
+}
---
+++ src/EmployeeManagement.Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [Required]        
+        [StringLength(100, MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        [MinLength(2)]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public DateTime DateOfBrith { get; set; }
+        public Gender Gender { get; set; }
+        public string PhotoPath { get; set; }
+
+        public int DepartmentId { get; set; }
+        public virtual Department Department { get; set; }
+
+
+    }
+}
